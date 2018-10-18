c5b1352c774f0a75e878e088a5c23c2b3348b855
Matthew Migliore <mmigliore311@gmail.com>
2018-10-18 18:40:52 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,9 +2,10 @@ require "pry"
 class Hash
   def keys_of(*arguments)
     # code goes here
-    
-    key_array = []
-    binding.pry
-    arguments.keys 
+  key_array = [] 
+  self.each do |item, value|
+    if value == arguments
+      key_array << item
+    end
   end
 end
